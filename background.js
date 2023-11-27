c7a6e50a42d240e146dcacb3a4b594e9df5ce2d3
Antony Kamau <kariuantony@gmail.com>
2023-11-27 08:07:21 +0000

    set runtime badge to ON

--- background.js
+++ background.js
@@ -3,7 +3,7 @@ const webstore = 'https://developer.chrome.com/docs/webstore'
 
 chrome.runtime.onInstalled.addListener(() => {
     chrome.action.setBadgeText({
-      text: "OFF",
+      text: "ON",
     });
 });
   
